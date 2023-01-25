b2a0b613cb638248c2fa61741d8f4f4bd1a7c3d3
CjordanIII <118429283+CjordanIII@users.noreply.github.com>
2023-01-25 02:26:02 -0500

    Non-focused update
    will work out kinks later I know I can make this work better but after I put the thank you screen I'm going to be done for now I pay for the program I learn from by a 6-month period and I would like to finish it. Will come back later when I finish it I did not abandon this.

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ let blkLine = document.getElementById('line-dark')
 let coBtn = document.getElementById('container-3')
 blkLine.style.display = 'none'
 coBtn.style.display ='none'
-let toggleContainerDisplay=document.getElementById('container-3')
-toggleContainerDisplay.style.display = 'none'
-document.getElementById('model').style.display = 'none'
+let toggleContainerDisplay=document.getElementById('model')
+toggleContainerDisplay.style.display='none'
+let Total = document.getElementById('Total')
 let prices = {}
-
 document.addEventListener("click",function(event){
     /* checks to see if the uuid matches what is clicked */
     if (event.target.dataset.uuid) {
@@ -19,9 +18,10 @@ document.addEventListener("click",function(event){
             let swapH2 = document.getElementById("check-out")
             let Name = data.name
             let Price = data.price
-            let Total = document.getElementById('Total')
+
             if (!prices[Name]) { /* checks if the name already exists in the prices object */
-                prices[Name] = Price;
+                prices[Name] = Price
+                
                 /* Groups of tollge makes sure YOUR ORDER appers once */
                     swapH2.classList.toggle('check-out')
                     blkLine.style.display = 'block'
@@ -33,21 +33,62 @@ document.addEventListener("click",function(event){
             } else {
                 prices[Name] += Price;
             }
+            console.log(prices)
             bottom.innerHTML = ""
             for (let item in prices) {
                 bottom.innerHTML += `
-                <div class = 'item'><span>${item}</span><span>$${prices[item]}</span></div>`
+                <div class = 'item'><span class='each-item'>${item}</span><sub class='remove'id='${item}'>Remove</sub>
+                <span class='price' >$${prices[item]}</span></div>`
+                let removebtn = document.getElementById(item);
+                removebtn.addEventListener('click', function() {/* only activates when you click delete */
+                    delete prices[item];
+                    bottom.innerHTML = "";
+                    for (let item in prices) {
+                        bottom.innerHTML += `
+                            <div class = 'item'><span class='each-item'>${item}</span><sub class='remove'id='${item}'>Remove</sub>
+                            <span class='price' >$${prices[item]}</span></div>`
+                        let removebtn = document.getElementById(item);
+                        removebtn.addEventListener('click', function() {
+                            delete prices[item];
+                            bottom.innerHTML = "";
+                            for (let item in prices) {
+                                bottom.innerHTML += `
+                                <div class = 'item'><span class='each-item'>${item}</span><sub class='remove'id='${item}'>Remove</sub>
+                                <span class='price' >$${prices[item]}</span></div>`
+                                let removebtn = document.getElementById(item);
+                                removebtn.addEventListener('click', function() {
+                                    delete prices[item];
+                                    console.log(prices);
+                                });
+                            }
+                        });
+                    }
+                });
+              
             }
+            /* this code is whwat happens druing codBtn and after you hit complete order */
             Total.textContent = `Total Price: $${sum(prices)}`
             coBtn.innerHTML = `
             <button class="complete-btn" id="complete-btn">Complete order</button>`
-            let CompleteOrder = document.getElementById('complete-btn')
-            CompleteOrder.addEventListener('click',function(){
-                toggleContainerDisplay.style.display = 'absolute'
-                document.getElementById('model').style.display = 'absolute'
-            })
             /* figure out how to make diplay for for the complete order btn and also fix above */
+            document.getElementById('complete-btn').addEventListener('click',function(){
+            toggleContainerDisplay.style.display='block'
+            
 
+
+            
+            const form = document.getElementById('submit')
+            form.addEventListener('click',function(){
+              let data= Array.from(document.querySelectorAll('.labels')).reduce((acc,input)=>({...acc,[input.id]: input.value}), {})
+              if(data.name&& data.email && data.ccn && data.cvc){
+                toggleContainerDisplay.style.display='none'
+                console.log(data)
+              }
+              /* I check to see if the data if filled or empty I creaded an object but did not do anythig with it. but now All I have to do is inside the if stament abmove  set the your order and all the info to thank you. Will deploye but I have to move on in the course sice im paying for months but i'm going to come back just need to move forward and make improvements later as i learn */
+            })
+
+
+            }) 
     }
     function dealWithMath(data){
     const filteredLIst = menuArray.filter(function(dataof){
@@ -59,9 +100,7 @@ document.addEventListener("click",function(event){
 }
     }
 
-
 })
-
 
 
 
